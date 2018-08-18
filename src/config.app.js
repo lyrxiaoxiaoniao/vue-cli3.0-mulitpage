2efbf6f243756a41f7c26b7faff99bca1c69b45a
liuyouren <liuyouren@shencom.cn>
2018-08-18 14:47:34 +0800

    update

--- src/config.app.js
+++ src/config.app.js
@@ -2,13 +2,13 @@
  * @fileOverview app配置
  * @author liuyouren
  * @date 2018/08/17
+ * APP_NAME的app对应相同位置的APP_LIST的配置，
+ * 这里APP_LIST对应了login和test两个应用的配置，
+ * BASE_URL为二级目录，也为根域上下文目录；INDEX_HTML代表了该应用的默认起始页，值为src/modules/下的模块目录名；
+ * CONTEXT_DIRECTORY为正常目录结构，类型为数组，数组元素为string时，对应该级下存在对应的以模块目录为名称的html文件，
+ * 数组元素为Object时，代表存在下级。
  */
 
-// APP_NAME的app对应相同位置的APP_LIST的配置，
-// 这里APP_LIST对应了login和test两个应用的配置，
-// BASE_URL为二级目录，也为根域上下文目录；INDEX_HTML代表了该应用的默认起始页，值为src/modules/下的模块目录名；
-// CONTEXT_DIRECTORY为正常目录结构，类型为数组，数组元素为string时，对应该级下存在对应的以模块目录为名称的html文件，
-// 数组元素为Object时，代表存在下级。
 module.exports = {
   APP_NAME: ['login', 'test'], // 应用名，按顺序对应APP_LIST元素位
   APP_LIST: [
